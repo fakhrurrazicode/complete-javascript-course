969557251cb35c3228760eef67d03dcf177028eb
superlativesecretsociety <superlativesecretsociety@gmail.com>
2023-07-05 14:47:43 +0700

    003 Function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,8 +1,29 @@
+/* ------------ 002 Activating Strict Mode ------------ */
 "use strict";
-
-let hasDriversLicence = false;
-const passTest = true;
+// let hasDriversLicence = false;
+// const passTest = true;
 
 // if (passTest) hasDriverLicence = true;
-if (passTest) hasDriversLicence = true;
-if (hasDriversLicence) console.log("I can drive :D");
+// if (hasDriversLicence) console.log("I can drive :D");
+
+/* ------------ 003 Functions ------------ */
+function logger() {
+  console.log("My name is razi");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  //   console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples dan ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
